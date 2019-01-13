a30c8c2df5259436d0a7d01cc6acf401550967f4
Zhou-wenbo <42958603+Zhou-wenbo@users.noreply.github.com>
2019-01-13 18:38:50 +0800

    Add files via upload

---
+++ test10/index.js
@@ -0,0 +1,49 @@
+// var regular_joe = 'I am global !';
+// function prison(){
+// 	var prsion = 'I am local !';
+// }
+// prsion();
+// console.log(regular_joe);
+// console.log(posioner);
+// let (prsioner= 'I am prsion!'){
+// 	console.log(prsioner);
+// }
+// console.log(prsioner);
+// function prison(){
+// 	prisoner_1='I have escaped!';
+// 	var prision_2='I am locked in!';
+// }
+// prison();
+// console.log(prisoner_1);
+// console.log(prision_2);
+// function prison(){
+// 	for(i=0; i<10;i++){
+
+// 	}
+// }
+// prison();
+// console.log("i");
+// delete window.i;
+// function prison(){
+// 	var i ;
+// 	for(i=0; i<10;i++){
+
+// 	}
+// }
+// prison();
+// console.log("i");
+// 变量提升
+// function prison(){
+// 	var prisoner;
+// 	console.log(prisoner);
+// 	var prisoner='Now I am defind!';
+// 	console.log(prisoner);
+// 	prisoner='prisoner assigned';
+// 	console.log(prisoner);
+// }
+// prison();
+// var regular_joe='Regular Joe';
+// function prison(){
+// 	console.log(regular_joe);
+// }
+// prison();
